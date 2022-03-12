dbb79c0c8620ccd25269be6273b94d9f0d2f2d72
Mihail Risteski <MihailRisteski@outlook.com>
2022-03-12 21:29:35 +0100

    added tables include

--- premake5.lua
+++ premake5.lua
@@ -16,7 +16,8 @@ project "ImGui"
         "imgui_rectpack.h",
         "imgui_textedit.h",
         "imgui_truetype.h",
-        "imgui_demo.cpp"
+        "imgui_demo.cpp",
+        "imgui_tables.cpp"
     }
 
     filter "system:windows"
